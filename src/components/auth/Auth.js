c445af5d5e110d723161f14b4a6e3140624d514d
Ulibka <gena68@inbox.ru>
2019-08-21 17:24:36 +0300

    стили

--- src/components/auth/Auth.js
+++ src/components/auth/Auth.js
@@ -1,70 +1,71 @@
 import React from "react";
-import styles from './Auth.css';
+import styles from './Auth.module.css';
 
 // className: styles.input,
 // https://github.com/firebase/firebaseui-web#demo
 // https://fir-ui-demo-84a6c.firebaseapp.com/
 
-class  AuthDialog extends React.Component {
+class AuthDialog extends React.Component {
     // constructor(props) {
     //     super(props);
     // };
 
-    render () {
-      return (
-        <div>
-            <div className="firebaseui-card-content">
-                AUTH Starts
+    render() {
+        let spanStyle = {
+            color: 'blue',
+            display : 'inline',
+            paddingLeft : '15px'
+        };
+
+        return (
+            <div>
+                <div className="firebaseui-card-content">
+                    AUTH Starts
 
 
                     <div>
-                        <button className={styles.firebaseuiIdpButton}>
-                            <span className="firebaseui-idp-icon-wrapper">
-                                <img className="firebaseui-idp-icon" alt=""
-                                     src="https://www.gstatic.com/firebasejs/ui/2.0.0/images/auth/google.svg">
-                                </img>
-                            </span>
-                            <span className="firebaseui-idp-text firebaseui-idp-text-long">Sign in with Google</span>
-                            <span className="firebaseui-idp-text firebaseui-idp-text-short">Google</span>
+                        <button>
+
+                            <img className={styles.ImgIcon} alt=""
+                                 src="https://www.gstatic.com/firebasejs/ui/2.0.0/images/auth/google.svg">
+                            </img>
+
+                            <span style={spanStyle}>Sign in with Google</span>
+
                         </button>
                     </div>
-                    <li >
-                        <button className="firebaseui-idp-button mdl-button mdl-js-button mdl-button--raised firebaseui-idp-facebook firebaseui-id-idp-button">
 
-                            <span className="firebaseui-idp-icon-wrapper">
-                                <img className="firebaseui-idp-icon" alt=""
-                                     src="https://www.gstatic.com/firebasejs/ui/2.0.0/images/auth/facebook.svg">
-                                </img>
-                            </span>
-                            <span className="firebaseui-idp-text firebaseui-idp-text-long">Sign in with Facebook</span>
-                            <span className="firebaseui-idp-text firebaseui-idp-text-short">Facebook</span>
+                    <div>
+                        <button>
+
+
+                            <img className={styles.ImgIcon + ' ' + styles.bgr1} alt=""
+                                 src="https://www.gstatic.com/firebasejs/ui/2.0.0/images/auth/facebook.svg">
+                            </img>
+
+                            <span style={spanStyle}>Sign in with Facebook</span>
+
                         </button>
-                    </li>
+                    </div>
 
 
-                    <li >
-                        <button
-                            className="firebaseui-idp-button mdl-button mdl-js-button mdl-button--raised firebaseui-idp-password firebaseui-id-idp-button">
+                    <div>
+                        <button>
+                            <img className={styles.ImgIcon + ' ' + styles.bgr2} alt=""
+                                 src="https://www.gstatic.com/firebasejs/ui/2.0.0/images/auth/mail.svg">
+                            </img>
+                            <span style={spanStyle}>Sign in with email</span>
 
-                            <span className="firebaseui-idp-icon-wrapper">
-                                <img className="firebaseui-idp-icon" alt=""
-                                     src="https://www.gstatic.com/firebasejs/ui/2.0.0/images/auth/mail.svg">
-                                </img>
-                            </span>
-                            <span
-                                className="firebaseui-idp-text firebaseui-idp-text-long">Sign in with email</span><span
-                            className="firebaseui-idp-text firebaseui-idp-text-short">Email</span></button>
-                    </li>
+                        </button>
+                    </div>
 
 
-
+                </div>
 
 
             </div>
-
-
-        </div>
-    );}
+        );
+    }
 }
 
 export default AuthDialog;
